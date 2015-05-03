86b554f52f9aec92b13bd377bf2efcb173a851e8
wzpziyi1 <1204473090@qq.com>
2015-05-03 15:58:20 +0800

    first commit

---
+++ Scrawl/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  Scrawl
+//
+//  Created by 王志盼 on 15/5/3.
+//  Copyright (c) 2015年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
---
+++ Scrawl/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  Scrawl
+//
+//  Created by 王志盼 on 15/5/3.
+//  Copyright (c) 2015年 王志盼. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
