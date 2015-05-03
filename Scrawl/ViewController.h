4ce08baaa54cc5456fcbb418288c069c4ee68491
wzpziyi1 <1204473090@qq.com>
2015-05-03 10:58:39 +0800

    Initial Commit

---
+++ Scrawl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scrawl
+//
+//  Created by 王志盼 on 15/5/3.
+//  Copyright (c) 2015年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Scrawl/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Scrawl
+//
+//  Created by 王志盼 on 15/5/3.
+//  Copyright (c) 2015年 王志盼. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
